fe61a90f47e960a2947c094f3ab78687a4691ec5
ElijahCoding <cuiyeqing960904@gmail.com>
2020-04-13 16:20:56 +0300

    request URL path

--- lenslocked.com/main.go
+++ lenslocked.com/main.go
@@ -6,7 +6,12 @@ import (
 )
 
 func handleFunc(w http.ResponseWriter, r *http.Request)  {
-	fmt.Fprint(w, "<h1>super welcome</h1>")
+	w.Header().Set("Content-Type", "text/html")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>super welcome</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "Mail me <a href=\"mailto:support@google.com\">support</a>")
+	}
 }
 
 func main()  {
