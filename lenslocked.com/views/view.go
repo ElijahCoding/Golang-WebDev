586d49cbf0837127d43609750b2629ab2dc524a4
ElijahCoding <cuiyeqing960904@gmail.com>
2020-04-14 17:46:33 +0300

    rendering layout

--- lenslocked.com/views/view.go
+++ lenslocked.com/views/view.go
@@ -2,8 +2,14 @@ package views
 
 import "html/template"
 
-func NewView(files ...string) *View {
-	files = append(files, "views/layouts/footer.gohtml")
+func NewView(layout string, files ...string) *View {
+	files = append(
+		files,
+		"views/layouts/bootstrap.gohtml",
+		"views/layouts/alert.gohtml",
+		"views/layouts/navbar.gohtml",
+		"views/layouts/footer.gohtml",
+		)
 
 	t, err := template.ParseFiles(files...)
 	if err != nil {
@@ -11,6 +17,7 @@ func NewView(files ...string) *View {
 	}
 	return &View{
 		Template: t,
+		Layout: layout,
 	}
 }
 
