351eb48d943af85b880758b611096e63a0a25958
ElijahCoding <cuiyeqing960904@gmail.com>
2020-04-15 17:25:31 +0300

    building signup page

--- lenslocked.com/main.go
+++ lenslocked.com/main.go
@@ -9,24 +9,37 @@ import (
 var (
 	homeView *views.View
 	contactView *views.View
+	signupView *Views.View
 )
 
 func home(w http.ResponseWriter, r *http.Request)  {
 	w.Header().Set("Content-Type", "text/html")
-	homeView.Render(w, nil)
+	must(homeView.Render(w, nil))
 }
 
 func contact(w http.ResponseWriter, r *http.Request)  {
 	w.Header().Set("Content-Type", "text/html")
-	contactView.Render(w, nil)
+	must(contactView.Render(w, nil))
+}
+
+func signup(w http.ResponseWriter, r *http.Request)  {
+	w.Header().Set("Content-Type", "text/html")
 }
 
 func main()  {
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap","views/contact.gohtml")
+	signupView = views.NewView("bootstrap","views/signup.gohtml")
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/signup", signup)
 	http.ListenAndServe(":3000", r)
+}
+
+func must(err error)  {
+	if err != nil {
+		panic(err)
+	}
 }
