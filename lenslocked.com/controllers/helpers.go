4ea216457e813cd409eea1de27b740088b628c35
ElijahCoding <cuiyeqing960904@gmail.com>
2020-04-16 20:30:52 +0300

    helper built

--- lenslocked.com/controllers/helpers.go
+++ lenslocked.com/controllers/helpers.go
@@ -5,6 +5,14 @@ import (
 	"github.com/gorilla/schema"
 )
 
-func main()  {
-	
+func parseForm(r *http.Request, dst interface{}) error {
+	if err := r.ParseForm(); err != nil {
+		return err
+	}
+
+	dec := schema.NewDecoder()
+	if err := dec.Decode(dst, r.PostForm); err != nil {
+		return err
+	}
+	return nil
 }
--- lenslocked.com/controllers/users.go
+++ lenslocked.com/controllers/users.go
@@ -2,7 +2,6 @@ package controllers
 
 import (
 	"fmt"
-	"github.com/gorilla/schema"
 	"gofullstack/lenslocked.com/views"
 	"net/http"
 )
@@ -29,12 +28,8 @@ type SignupForm struct {
 }
 
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
-		panic(err)
-	}
-	dec := schema.NewDecoder()
 	var form SignupForm
-	if err := dec.Decode(&form, r.PostForm); err != nil {
+	if err := parseForm(r, &form); err != nil {
 		panic(err)
 	}
 	fmt.Fprint(w, form)
