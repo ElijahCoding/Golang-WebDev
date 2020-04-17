79772bf5274f24913961ee50ed7de9089b5184c6
ElijahCoding <cuiyeqing960904@gmail.com>
2020-04-17 17:06:22 +0300

    refactored

--- lenslocked.com/controllers/static.go
+++ lenslocked.com/controllers/static.go
@@ -2,3 +2,14 @@ package controllers
 
 import "gofullstack/lenslocked.com/views"
 
+func NewStatic() *Static {
+	return &Static{
+		Home: views.NewView("bootstrap", "views/static/home.gohtml"),
+		Contact: views.NewView("bootstrap", "views/static/contact.gohtml"),
+	}
+}
+
+type Static struct {
+	Home *views.View
+	Contact *views.View
+}
