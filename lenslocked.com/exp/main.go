cd73c414c076b62bec11319330154ed325a1264c
ElijahCoding <cuiyeqing960904@gmail.com>
2020-04-20 22:44:50 +0300

    connected to db

--- lenslocked.com/exp/main.go
+++ lenslocked.com/exp/main.go
@@ -3,9 +3,29 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"githuub.com/lib/pq"
+	_ "github.com/lib/pq"
+)
+
+const (
+	host = "localhost"
+	port = 54320
+	user = "postgres"
+	password = "root"
+	dbname = "lenslocked.com"
 )
 
 func main() {
-	
+	psqlInfo := fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("connected !")
+	db.Close()
 }
