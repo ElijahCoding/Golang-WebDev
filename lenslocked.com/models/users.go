2dae3373f233925713b52c01028e1f71c569b82e
ElijahCoding <cuiyeqing960904@gmail.com>
2020-04-21 17:37:33 +0300

    user model built

--- lenslocked.com/models/users.go
+++ lenslocked.com/models/users.go
@@ -1,5 +1,12 @@
-package main
+package models
 
-func main() {
-	
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+type User struct {
+	gorm.Model
+	Name string
+	Email string `gorm:"not null;unique_index"`
 }
