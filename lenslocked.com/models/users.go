3958f03b40c71e1d75efe86eef725c4f241bb818
ElijahCoding <cuiyeqing960904@gmail.com>
2020-04-21 22:43:45 +0300

    connect to db

--- lenslocked.com/models/users.go
+++ lenslocked.com/models/users.go
@@ -1,9 +1,45 @@
 package models
 
 import (
+	"errors"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
+
+var (
+	ErrNotFound = errors.New("models: resource not found.")
+)
+
+func NewUserService(connectionInfo string) (*UserService, error) {
+	db, err := gorm.Open("postgres", connectionInfo)
+	if err != nil {
+		return nil, err
+	}
+	return &UserService{
+		db: db,
+	}, nil
+}
+
+type UserService struct {
+	db *gorm.DB
+}
+
+func (us *UserService) ByID(id uint) (*User, error) {
+	var user User
+	err := us.db.Where("id = ?", id).First(&user).Error
+	switch err {
+	case nil:
+		return &user, nil
+	case gorm.ErrRecordNotFound:
+		return nil, ErrNotFound
+	default:
+		return nil, err
+	}
+}
+
+func (us *UserService) Close() error {
+
+}
 
 type User struct {
 	gorm.Model
