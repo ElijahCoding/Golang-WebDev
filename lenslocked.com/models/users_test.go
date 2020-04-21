83f09383f828065d06dc9841cd61c700118234f1
ElijahCoding <cuiyeqing960904@gmail.com>
2020-04-22 00:08:05 +0300

    testing creating user

--- lenslocked.com/models/users_test.go
+++ lenslocked.com/models/users_test.go
@@ -1,5 +1,30 @@
-package main
+package models
 
-func main() {
-	$END$
+import (
+	"fmt"
+	"testing"
+)
+
+func testingUserService() (*UserService, error) {
+	const (
+		host = "localhost"
+		port = 54320
+		user = "postgres"
+		password = "root"
+		dbname = "lenslocked_test"
+	)
+	psqlInfo := fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	us, err := NewUserService(psqlInfo)
+	if err != nil {
+		return nil, err
+	}
+	us.db.LogMode(false)
+	us.DestructiveReset()
+	return us, nil
+}
+
+func TestCreateUser(t *testing.T)  {
+	
 }
