7a8f9e51f80c33326752db81fa90cb2d0c7d1066
ElijahCoding <cuiyeqing960904@gmail.com>
2020-04-22 14:00:57 +0300

    users test finished

--- lenslocked.com/models/users_test.go
+++ lenslocked.com/models/users_test.go
@@ -3,6 +3,7 @@ package models
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
 func testingUserService() (*UserService, error) {
@@ -26,5 +27,26 @@ func testingUserService() (*UserService, error) {
 }
 
 func TestCreateUser(t *testing.T)  {
-	
+	us, err := testingUserService()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	user := User{
+		Name: "test",
+		Email: "test@test.com",
+	}
+	err = us.Create(&user)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if user.ID == 0 {
+		t.Errorf("Expected ID > 0. Received %d", user.ID)
+	}
+	if time.Since(user.CreatedAt) > time.Duration(5 * time.Second) {
+		t.Errorf("Expected CreatedAt to be recent. Received %s", user.CreatedAt)
+	}
+	if time.Since(user.UpdatedAt) > time.Duration(5 * time.Second) {
+		t.Errorf("Expected UpdatedAt to be recent. Received %s", user.UpdatedAt)
+	}
 }
