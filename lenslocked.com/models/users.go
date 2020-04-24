702828902f40615c07c70fdcfab913714114656e
ElijahCoding <cuiyeqing960904@gmail.com>
2020-04-24 04:55:45 +0300

    find user by remember hash

--- lenslocked.com/models/users.go
+++ lenslocked.com/models/users.go
@@ -49,6 +49,16 @@ func (us *UserService) ByEmail(email string) (*User, error) {
 	return &user, err
 }
 
+func (us *UserService) ByRemember(token string) (*User, error) {
+	var user User
+	rememberHash := us.hmac.Hash(token)
+	err := first(us.db.Where("remember_hash = ?", rememberHash), &user)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
 func (us *UserService) Authenticate(email, password string) (*User, error) {
 	foundUser, err := us.ByEmail(email)
 	if err != nil {
