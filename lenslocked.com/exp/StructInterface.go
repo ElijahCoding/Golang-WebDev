4ffdaa4326b91a4e681fe315135db165c6cfa877
ElijahCoding <cuiyeqing960904@gmail.com>
2020-04-27 15:10:48 +0300

    cat dog example

--- lenslocked.com/exp/StructInterface.go
+++ lenslocked.com/exp/StructInterface.go
@@ -1,1 +1,28 @@
 package main
+
+import "fmt"
+
+type Cat struct {}
+
+func (c Cat) Speak() {
+	fmt.Println("meow")
+}
+
+type Dog struct {}
+
+func (d Dog) Speak() {
+	fmt.Println("woof")
+}
+
+type Husky struct {
+	Speaker
+}
+
+type Speaker interface {
+	Speak()
+}
+
+func main()  {
+	h := Husky{Cat{}}
+	h.Speak()
+}
