1a85101837704a2e1741f391466c7686744163a2
ElijahCoding <cuiyeqing960904@gmail.com>
2020-04-27 19:53:05 +0300

    refactoring to book model

--- books-list/main.go
+++ books-list/main.go
@@ -1,43 +1,5 @@
 package main
 
-import (
-	"github.com/gorilla/mux"
-	"log"
-	"net/http"
-)
-
-type Book struct {
-	ID int `json:id`
-	Title string `json:title`
-	Author string `json:author`
-	Year string `json:year`
-}
-
-var book []Book
-
 func main()  {
-	router := mux.NewRouter()
-
-	router.HandleFunc("/books", getBooks).Methods("GET")
-	router.HandleFunc("/books/{id}", getBook).Methods("GET")
-	router.HandleFunc("/books", addBook).Methods("POST")
-	router.HandleFunc("/books", updateBook).Methods("PUT")
-	router.HandleFunc("/books/{id}", removeBook).Methods("DELETE")
-
-	log.Fatal(http.ListenAndServe(":8000", router))
-}
-
-func getBooks(w http.ResponseWriter, r *http.Request) {
-}
-
-func getBook(w http.ResponseWriter, r *http.Request) {
-}
-
-func addBook(w http.ResponseWriter, r *http.Request) {
-}
-
-func updateBook(w http.ResponseWriter, r *http.Request) {
-}
-
-func removeBook(w http.ResponseWriter, r *http.Request) {
+	
 }
