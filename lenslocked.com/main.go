dce0711dac107a09006d1f611358107a46cdc63e
ElijahCoding <cuiyeqing960904@gmail.com>
2020-04-27 18:56:22 +0300

    code refactored

--- lenslocked.com/main.go
+++ lenslocked.com/main.go
@@ -16,32 +16,30 @@ const (
 	dbname = "lenslocked.com"
 )
 
-func main()  {
-	psqlInfo := fmt.Sprintf(
-		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+func main() {
+	// Create a DB connection string and then use it to
+	// create our model services.
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
-
-	us, err := models.NewUserService(psqlInfo)
-	must(err)
-	defer us.Close()
-	us.AutoMigrate()
+	ug, err := models.NewUserGorm(psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	ug.AutoMigrate()
 
 	staticC := controllers.NewStatic()
-	usersC := controllers.NewUsers(us)
+	usersC := controllers.NewUsers(ug)
 
 	r := mux.NewRouter()
 	r.Handle("/", staticC.Home).Methods("GET")
 	r.Handle("/contact", staticC.Contact).Methods("GET")
-	r.Handle("/signup", usersC.NewView).Methods("GET")
+	r.HandleFunc("/signup", usersC.New).Methods("GET")
 	r.HandleFunc("/signup", usersC.Create).Methods("POST")
 	r.Handle("/login", usersC.LoginView).Methods("GET")
 	r.HandleFunc("/login", usersC.Login).Methods("POST")
 	r.HandleFunc("/cookietest", usersC.CookieTest).Methods("GET")
-	http.ListenAndServe(":3000", r)
-}
-
-func must(err error)  {
-	if err != nil {
+	if err := http.ListenAndServe(":3000", r); err != nil {
 		panic(err)
 	}
 }
