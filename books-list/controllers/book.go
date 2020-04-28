ee6e183c633259cf27a9739f5bc4c39ffdbd2bdd
ElijahCoding <cuiyeqing960904@gmail.com>
2020-04-28 23:38:19 +0300

    CRUD methods added

--- books-list/controllers/book.go
+++ books-list/controllers/book.go
@@ -1,8 +1,10 @@
 package controllers
 
 import (
+	"database/sql"
 	"gofullstack/books-list/models"
 	"log"
+	"net/http"
 )
 
 var books []models.Book
@@ -12,5 +14,35 @@ type Controller struct{}
 func logFatal(err error)  {
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func (c Controller) GetBooks(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
+
+func (c Controller) GetBook(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
+
+func (c Controller) AddBook(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
+
+func (c Controller) UpdateBook(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
+
+func (c Controller) RemoveBook(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
 	}
 }
