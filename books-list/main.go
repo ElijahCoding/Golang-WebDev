3ef7fa854dc09c3cd94eed2aca9bbe431f7cba21
ElijahCoding <cuiyeqing960904@gmail.com>
2020-04-29 13:44:34 +0300

    main.go built, building controller methods

--- books-list/main.go
+++ books-list/main.go
@@ -5,7 +5,10 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/subosito/gotenv"
 	"gofullstack/books-list/models"
+	"gofullstack/books-list/driver"
+	"gofullstack/books-list/controllers"
 	"log"
+	"net/http"
 )
 
 var books []models.Book
@@ -26,6 +29,33 @@ type WithCORS struct {
 	r *mux.Router
 }
 
+func (s *WithCORS) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	if origin := req.Header.Get("Origin"); origin != "" {
+		res.Header().Set("Access-Control-Allow-Origin", origin)
+		res.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		res.Header().Set("Access-Control-Allow-Headers",
+			"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	}
+
+	// Stop here for a Preflighted OPTIONS request.
+	if req.Method == "OPTIONS" {
+		return
+	}
+	// Lets Gorilla work
+	s.r.ServeHTTP(res, req)
+}
+
 func main()  {
-	
+	db = driver.ConnectDB()
+	router := mux.NewRouter()
+
+	controller := controllers.Controller{}
+
+	router.HandleFunc("/books", controller.GetBooks(db)).Methods("GET")
+	router.HandleFunc("/books/{id}", controller.GetBook(db)).Methods("GET")
+	router.HandleFunc("/books", controller.AddBook(db)).Methods("POST")
+	router.HandleFunc("/books", controller.UpdateBook(db)).Methods("PUT")
+	router.HandleFunc("/books/{id}", controller.RemoveBook(db)).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", &WithCORS{router}))
 }
