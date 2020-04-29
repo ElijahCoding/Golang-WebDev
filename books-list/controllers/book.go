d8294e67a645725b850c867ff09a55ef359098d5
ElijahCoding <cuiyeqing960904@gmail.com>
2020-04-29 14:11:15 +0300

    controller built

--- books-list/controllers/book.go
+++ books-list/controllers/book.go
@@ -2,9 +2,13 @@ package controllers
 
 import (
 	"database/sql"
+	"encoding/json"
+	"github.com/gorilla/mux"
 	"gofullstack/books-list/models"
+	bookRepository "gofullstack/books-list/repository/book"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 var books []models.Book
@@ -19,30 +23,69 @@ func logFatal(err error)  {
 
 func (c Controller) GetBooks(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		var book models.Book
+		books = []models.Book{}
+		bookRepo := bookRepository.BookRepository{}
 
+		books = bookRepo.GetBooks(db, book, books)
+
+		json.NewEncoder(w).Encode(books)
 	}
 }
 
 func (c Controller) GetBook(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		var book models.Book
+		params := mux.Vars(r)
 
+		books := []models.Book{}
+		bookRepo := bookRepository.BookRepository{}
+
+		id, err := strconv.Atoi(params["id"])
+		logFatal(err)
+
+		book = bookRepo.GetBook(db, book, id)
+
+		json.NewEncoder(w).Encode(book)
 	}
 }
 
 func (c Controller) AddBook(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		var book models.Book
+		var bookID int
 
+		json.NewDecoder(r.Body).Decode(&book)
+
+		bookRepo := bookRepository.BookRepository{}
+		bookID = bookRepo.AddBook(db, book)
+
+		json.NewEncoder(w).Encode(bookID)
 	}
 }
 
 func (c Controller) UpdateBook(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		var book models.Book
+		json.NewDecoder(r.Body).Decode(&book)
 
+		bookRepo := bookRepository.BookRepository{}
+		rowsUpdated := bookRepo.UpdateBook(db, book)
+
+		json.NewEncoder(w).Encode(rowsUpdated)
 	}
 }
 
 func (c Controller) RemoveBook(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		params := mux.Vars(r)
+		bookRepo := bookRepository.BookRepository{}
 
+		id, err := strconv.Atoi(params["id"])
+		logFatal(err)
+
+		rowsDeleted := bookRepo.RemoveBook(db, id)
+
+		json.NewEncoder(w).Encode(rowsDeleted)
 	}
 }
