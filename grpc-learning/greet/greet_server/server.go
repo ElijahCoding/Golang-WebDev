15cd2b96199004c5d271bc315ca162aab0520fdb
ElijahCoding <cuiyeqing960904@gmail.com>
2020-04-30 13:07:28 +0300

    grpc server boilerplate built, preparing to build client side

--- grpc-learning/greet/greet_server/server.go
+++ grpc-learning/greet/greet_server/server.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"fmt"
+	"gofullstack/grpc-learning/greet/greetpd"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+type server struct {}
+
+func main()  {
+	fmt.Println("working")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+
+	if err != nil {
+		log.Fatal("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	greetpd.RegisterGreetServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
