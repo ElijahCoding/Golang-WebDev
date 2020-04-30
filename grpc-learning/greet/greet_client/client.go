ec5e0683074ce6bf35b52073ccfc2a9099c3f06f
ElijahCoding <cuiyeqing960904@gmail.com>
2020-04-30 13:23:12 +0300

    client side built

---
+++ grpc-learning/greet/greet_client/client.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"gofullstack/grpc-learning/greet/greetpd"
+	"google.golang.org/grpc"
+	"log"
+)
+
+func main()  {
+	fmt.Println("client side")
+
+	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Could not connect: %v", err)
+	}
+
+	defer cc.Close()
+
+	greetpd.NewGreetServiceClient(cc)
+	fmt.Println("Created client")
+}
