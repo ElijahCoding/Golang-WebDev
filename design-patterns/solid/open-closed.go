65b8eafb44585778451ccdf1c96698ce02086d13
ElijahCoding <cuiyeqing960904@gmail.com>
2020-04-30 17:23:29 +0300

    open closed principle template set up

---
+++ design-patterns/solid/open-closed.go
@@ -0,0 +1,47 @@
+package main
+
+type Color int
+
+const (
+	red Color = iota
+	green
+	blue
+)
+
+type Size int
+
+const (
+	small Size = iota
+	medium
+	large
+)
+
+type Product struct {
+	name string
+	color Color
+	size Size
+}
+
+type Filter struct {
+
+}
+
+func (f *Filter) FilterByColor(products []Product, color Color) []*Product {
+	result := make([]*Product, 0)
+
+	for i, v := range products {
+		if v.color == color {
+			result = append(result, &products[i])
+		}
+	}
+
+	return result
+}
+
+func main()  {
+	apple := Product{"Apple", green, small}
+	tree := Product{"Tree", green, large}
+	house := Product{"House", blue, large}
+
+	products := []Product{apple, tree, house}
+}
