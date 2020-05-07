e47788d1a1c3822d497eda63dcb4bb78d2e49604
ElijahCoding <cuiyeqing960904@gmail.com>
2020-05-07 18:20:06 +0300

    mvc structure in golang

--- microservices/introduction/webserver/main.go
+++ microservices/introduction/webserver/main.go
@@ -1,1 +1,13 @@
 package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("/hello", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte("hello"))
+	})
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+}
