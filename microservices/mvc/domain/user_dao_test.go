2c13521b8deca2658db5610f1305bb6964d79800
ElijahCoding <cuiyeqing960904@gmail.com>
2020-05-08 14:09:30 +0300

    get user with no error test built

--- microservices/mvc/domain/user_dao_test.go
+++ microservices/mvc/domain/user_dao_test.go
@@ -11,6 +11,19 @@ func TestGetUserNoUserFound(t *testing.T) {
 
 	assert.Nil(t, user, "we were not expecting a user with id 0")
 	assert.NotNil(t, err, "we were expeccting an error when user id is 0")
+
 	assert.EqualValues(t, err.StatusCode, http.StatusNotFound)
 	assert.EqualValues(t, "not found", err.Code)
+}
+
+func TestGetUserNoError(t *testing.T) {
+	user, err := GetUser(123)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, user)
+
+	assert.EqualValues(t, 123, user.Id)
+	assert.EqualValues(t, "Fede", user.FirstName)
+	assert.EqualValues(t, "Leon", user.LastName)
+	assert.EqualValues(t, "myemail@gmail.com", user.Email)
 }
