8c9cd30bb9388180176f71a413fd6b37e83f0764
ElijahCoding <cuiyeqing960904@gmail.com>
2020-07-17 14:09:14 +0300

    building restful-mock service

--- microservices/mvc/app/app.go
+++ microservices/mvc/app/app.go
@@ -3,9 +3,19 @@ package app
 import (
 	"gofullstack/microservices/mvc/controllers"
 	"net/http"
+	"github.com/gin-gonic/gin"
 )
 
+var (
+	router *gin.Engine
+)
+
+func init() {
+	router = gin.Default()
+}
+
 func StartApp()  {
+	mapUrls()
 	http.HandleFunc("/users", controllers.GetUser)
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
